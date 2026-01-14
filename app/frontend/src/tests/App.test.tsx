bc195c805c218ee1c6bafc216df1312a90f068ee
Azer <azka8805@bht-berlin.de>
2026-01-14 23:10:53 +0100

    my app test update

--- app/frontend/src/tests/App.test.tsx
+++ app/frontend/src/tests/App.test.tsx
@@ -6,18 +6,22 @@ describe("App UI", () => {
 
   it("renders the main title", () => {
     render(<App />)
-    expect(screen.getByText(/Search Pokémon/i)).toBeDefined()
+    expect(screen.getByText(/Welcome to the Pokémon App/i)).toBeDefined()
   })
 
-  it("renders the search input", () => {
+  it("renders navigation buttons", () => {
     render(<App />)  
-    const input = screen.getByPlaceholderText(/Search Pokémon/i)
-    expect(input).toBeDefined()
+    const createTeamBtn = screen.getByRole("button", { name: /Create Team/i })
+    const compareBtn = screen.getByRole("button", { name: /Compare Pokémons/i })
+    const searchBtn = screen.getByRole("button", { name: /Search a Pokeman/i })
+    expect(createTeamBtn).toBeDefined()
+    expect(compareBtn).toBeDefined()
+    expect(searchBtn).toBeDefined()
   })
 
   it("renders the Search button", () => {
     render(<App />)
-    const button = screen.getByRole("button", { name: /search/i })
+    const button = screen.getByRole("button", { name: /Search a Pokeman/i })
     expect(button).toBeDefined()
   })
 
